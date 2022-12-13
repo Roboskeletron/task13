adb7297d445d494de212ff8889d0269933a8cc43
Roboskeletron <wwwrt287@gmail.com>
2022-12-13 16:18:44 +0300

    draw 1 character

--- src/main/java/com/roboskeletron/task13/controllers/GameController.java
+++ src/main/java/com/roboskeletron/task13/controllers/GameController.java
@@ -2,15 +2,16 @@ package com.roboskeletron.task13.controllers;
 
 import com.roboskeletron.task13.Entities.Character;
 import com.roboskeletron.task13.Render;
-import com.roboskeletron.task13.base.Entity;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.roboskeletron.task13.primitives.Point2D;
+import com.roboskeletron.task13.primitives.Sprite;
 
 public class GameController {
     private final Render render;
 
     public GameController(Render render){
         this.render = render;
+        var entitiesList = render.getEntitiesList();
+        entitiesList.add(new Character(new Point2D(50, 50), new Sprite(0), "character0"));
+        render.drawFrame();
     }
 }
