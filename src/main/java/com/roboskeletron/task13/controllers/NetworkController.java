95202c12b79ba6fb81f3df88ea7d7bde06a58263
Roboskeletron <wwwrt287@gmail.com>
2022-12-16 23:45:12 +0300

    Implements IInput and update

--- src/main/java/com/roboskeletron/task13/controllers/NetworkController.java
+++ src/main/java/com/roboskeletron/task13/controllers/NetworkController.java
@@ -1,6 +1,8 @@
 package com.roboskeletron.task13.controllers;
 
 import com.roboskeletron.task13.Entities.Player;
+import com.roboskeletron.task13.interfaces.IInput;
+import com.roboskeletron.task13.primitives.NetworkPackage;
 import com.roboskeletron.task13.primitives.Point2D;
 import com.roboskeletron.task13.primitives.Sprite;
 
@@ -10,12 +12,14 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class NetworkController {
+public class NetworkController implements IInput {
     public final boolean isSever;
     private final InputStream inputStream;
     private final OutputStream outputStream;
     private  final Socket socket;
+    private byte inputInfo = 0;
     private Player player;
+    private NetworkPackage networkPackage = new NetworkPackage(1);
 
     public NetworkController(int port) throws IOException {
         isSever = true;
@@ -44,5 +48,27 @@ public class NetworkController {
 
     public Player getPlayer(){
         return player;
+    }
+
+    public void update(KeyController input) throws IOException {
+        inputInfo = networkPackage.readPackage(inputStream)[0];
+        player.update(this);
+
+        networkPackage.sendPackage(input.formPackage(), outputStream);
+    }
+
+    @Override
+    public boolean up() {
+        return (inputInfo & 4) == 1;
+    }
+
+    @Override
+    public boolean forward() {
+        return (inputInfo & 1) == 1;
+    }
+
+    @Override
+    public boolean back() {
+        return (inputInfo & 2) == 1;
     }
 }
