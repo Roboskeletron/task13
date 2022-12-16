dbb768f9a56f3db88d028699b6649567be9a4b82
Roboskeletron <wwwrt287@gmail.com>
2022-12-16 23:44:26 +0300

    Inherits IInput

--- src/main/java/com/roboskeletron/task13/controllers/KeyController.java
+++ src/main/java/com/roboskeletron/task13/controllers/KeyController.java
@@ -1,17 +1,46 @@
 package com.roboskeletron.task13.controllers;
 
+import com.roboskeletron.task13.interfaces.IInput;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 import java.util.HashSet;
 
-public class KeyController {
-    public static HashSet<KeyCode> keyInfo = new HashSet<>();
-    public static void onKeyPressed(KeyEvent keyEvent) {
+public class KeyController implements IInput {
+    public HashSet<KeyCode> keyInfo = new HashSet<>();
+    public void onKeyPressed(KeyEvent keyEvent) {
         keyInfo.add(keyEvent.getCode());
     }
 
-    public static void onKeyReleased(KeyEvent keyEvent) {
+    public void onKeyReleased(KeyEvent keyEvent) {
         keyInfo.remove(keyEvent.getCode());
+    }
+
+    @Override
+    public boolean up() {
+        return keyInfo.contains(KeyCode.W);
+    }
+
+    @Override
+    public boolean forward() {
+        return keyInfo.contains(KeyCode.D);
+    }
+
+    @Override
+    public boolean back() {
+        return keyInfo.contains(KeyCode.A);
+    }
+
+    public byte[] formPackage(){
+        byte data = 0;
+
+        if (up())
+            data+=4;
+        if (forward())
+            data+=1;
+        if (back())
+            data+=2;
+
+        return new byte[]{data};
     }
 }
